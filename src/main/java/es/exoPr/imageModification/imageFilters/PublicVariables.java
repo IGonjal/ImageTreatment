7d82b9535a8a1ee58e48277748770bbbb2f702c7
ismael.gonjal <ismael.gonjal@baratz.es>
2018-12-05 12:58:02 +0100

    arreglado bug con los canales, añadido método para restaurar variables por defecto

--- src/main/java/es/exoPr/imageModification/imageFilters/PublicVariables.java
+++ src/main/java/es/exoPr/imageModification/imageFilters/PublicVariables.java
@@ -10,8 +10,11 @@ public class PublicVariables {
 	public static double getThresholdColor() {
 		return THRESHOLD_COLOR;
 	}
+	public static void restoreDefaultThreshold() {
+		THRESHOLD_COLOR=DEFAULT_THRESHOLD_COLOR;
+	}
 	
-	private static final double DEFAULT_MAX_COLOR = 255d;
+	private static final double DEFAULT_MAX_COLOR = 0d;
 	private static double MAX_COLOR = DEFAULT_MAX_COLOR;
 	public static void setMaxColor(double neww) {
 		MAX_COLOR = neww;
@@ -19,8 +22,11 @@ public class PublicVariables {
 	public static double getMaxColor() {
 		return MAX_COLOR;
 	}
+	public static void restoreDefaultMaxColor() {
+		MAX_COLOR=DEFAULT_MAX_COLOR;
+	}
 	
-	private static final double DEFAULT_MIN_COLOR = 0d;
+	private static final double DEFAULT_MIN_COLOR = 255d;
 	private static double MIN_COLOR = DEFAULT_MIN_COLOR;
 	public static void setMinColor(double neww) {
 		MIN_COLOR = neww;
@@ -28,15 +34,21 @@ public class PublicVariables {
 	public static double getMinColor() {
 		return MIN_COLOR;
 	}
+	public static void restoreDefaultMincolor() {
+		MIN_COLOR=DEFAULT_MIN_COLOR;
+	}
 	
 	private static boolean[] DEFAULT_CHANNELS = {true,true,true};
 	private static boolean[] CHANNELS = DEFAULT_CHANNELS;
 	public static void setChannels(boolean[] neww) {
-		if(neww.length!= 3) {
+		if(neww.length== 3) {
 			CHANNELS = neww;
 		}
 	}
 	public static boolean[] getChannels() {
 		return CHANNELS;
+	}
+	public static void restoreDefaultChannels() {
+		CHANNELS=DEFAULT_CHANNELS;
 	}
 }
--- src/main/java/es/mine/imageModification/main/MainImagesModification.java
+++ src/main/java/es/mine/imageModification/main/MainImagesModification.java
@@ -1,9 +1,12 @@
 package es.mine.imageModification.main;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Arrays;
 
 import org.opencv.core.Mat;
-import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 
 import es.exoPr.imageModification.imageFilters.FilterUser;
@@ -19,11 +22,16 @@ public class MainImagesModification {
 		
 		FilterUser filterUser = new FilterUser();
 		
+		try (FileOutputStream fos = new FileOutputStream(new File("C:\\Users\\ismael.gonjal\\TrataImagenes\\ORIGINAL.jpg"));){
+			Files.copy(new File("c:\\image.jpg").toPath(), fos);
+		} catch (IOException e) {
+		}
+		
 		// 7 al ser un número de 3 bits
-		for(int i = 0; i <= 7; i++) {
-			boolean[] channels = {(i&1)==1,(i&3)==3, (i&5)==5};
-			System.out.println(Arrays.toString(channels));
-			for(ThresholdFilter t : ThresholdFilter.values()) {
+		for(ThresholdFilter t : ThresholdFilter.values()) {
+			for(int i = 1; i <= 7; i++) {
+				boolean[] channels = {(i&4)==4, (i&2)==2, (i&1)==1};
+				System.out.println(Arrays.toString(channels) + " - "+ i +" - "+ Integer.toBinaryString(i) + " - " + t.getName());
 				Mat destiny = filterUser.use(origin,t, channels);
 				Imgcodecs.imwrite("C:\\Users\\ismael.gonjal\\TrataImagenes\\"+t.getName().replace(" ", "") +"_" +i +".jpg", destiny);
 			}
